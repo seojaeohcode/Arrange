f6c120f702c27e1b53c5f9bf01bbc5c199ec6b9e
seojaeoh <seojaeohcoder@gmail.com>
2025-05-20 20:47:25 +0900

    update_test

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from mangum import Mangum
 
 app = FastAPI()
 
-@app.get("/health")
+
+@app.get("/health-check")
 async def health_check():
     return {"status": "ok"}
+
 
 handler = Mangum(app)  # Lambda용 엔트리포인트
