28ab97f0524b0abc44655d8624cf82a1821a1111
seojaeoh <seojaeohcoder@gmail.com>
2025-06-12 06:10:08 +0900

    test

--- backend/main.py
+++ backend/main.py
@@ -189,7 +189,7 @@ async def process_bookmarks(data: InputList):
 
 @app.get("/health-check")
 def health_check():
-    return {"status": "ok"}
+    return {"status": "okok"}
 
 # @app.post("/cluster")
 # def cluster(data: InputList):
